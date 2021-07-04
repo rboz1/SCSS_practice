5d23d7071c54ab14239ac1198353189da9f39d17
rboz1 <63253421+rboz1@users.noreply.github.com>
2021-07-04 11:03:07 -0400

    updated functions

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ let toggleMenu = () => {
   let menu = document.getElementsByClassName("toggle-menu");
 
   if (menu[0].style.display === "" || menu[0].style.display === "none"){
-      menu[0].style.display = "flex";
-      }else{
-        menu[0].style.display = "none";
-      }
+    return menu[0].style.display = "flex";
+    }else{
+      return menu[0].style.display = "none";
+  }
 }
 
 /*Prevents desktop navbar from disappearing after mobile hamburger menu is toggled 
@@ -17,9 +17,9 @@ let displayLargeNav = () => {
   let menu = document.getElementsByClassName("toggle-menu");
 
   if (width >= 752){
-    menu[0].style.display = "flex";
+    return menu[0].style.display = "flex";
   }else{
-    menu[0].style.display = "none";
+    return menu[0].style.display = "none";
   }
 }
 
